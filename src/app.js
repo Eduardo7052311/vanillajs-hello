a13835db1bb51161e85f16f09bb48bb8fe5304bb
Eduardo7052311 <124112815+Eduardo7052311@users.noreply.github.com>
2024-06-15 03:33:31 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying",
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateExcuse() {
+  const randomWho = getRandomElement(who);
+  const randomAction = getRandomElement(action);
+  const randomWhat = getRandomElement(what);
+  const randomWhen = getRandomElement(when);
+
+  const excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  document.getElementById("excuse").textContent = excuse;
+}
